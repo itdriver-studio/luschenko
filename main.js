87c33fc783df19bf919c62be87e214531be74bd8
Dmitrii <itdriver@yahoo.com>
2023-09-25 16:15:04 +0300

    typeof

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ let i1 = document.querySelector('.i1');
 let b1 = document.querySelector('.b1');
 
 b1.onclick = () => {
-    console.log(i1.value);
+    console.log(+i1.value + 10);
     i1.value = '';
 }
