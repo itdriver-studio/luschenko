b5f7b84140027e214bf2d4532e6b5cc9e2beff2a
Dmitrii <itdriver@yahoo.com>
2023-09-25 16:25:05 +0300

    parseInt

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let i1 = document.querySelector('.i1');
 let b1 = document.querySelector('.b1');
 
 b1.onclick = () => {
-    console.log(+i1.value + 10);
+    console.log(+i1.value);
+    console.log(parseInt(i1.value));
     i1.value = '';
 }
