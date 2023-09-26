661bb5ccbb72179211509bf30d84c17a23011695
Dmitrii <itdriver@yahoo.com>
2023-09-26 21:46:11 +0300

    if

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-let i1 = document.querySelector('.i1');
-let b1 = document.querySelector('.b1');
+let a = 9;
 
-b1.onclick = () => {
-    console.log(+i1.value);
-    console.log(parseInt(i1.value));
-    i1.value = '';
+if (a > 0) {
+    console.log('true');
 }
