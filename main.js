b531ef10ca369801c5572994a32a617a14bc05eb
Dmitrii <itdriver@yahoo.com>
2023-10-18 00:27:24 +0300

    dom

--- main.js
+++ main.js
@@ -1,20 +1,5 @@
 let d1 = document.querySelector('.d1');
-let c = 0;
-let res = '';
-let counter = 3;
-while (c < 4) {
-    let s = 0;
-    while (s < 4) {
-        if (counter > s) {
-            res += '-';
-        } else {
-            res += '*';
-        }
-        s++;
-    }
-    res += '<br>';
-    counter--;
-    c++;
-}
 
-d1.innerHTML = res;
+d1.style.width = '150px';
+d1.style.paddingBottom = '150px';
+// d1.style.backgroundColor = '#000';
