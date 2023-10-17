62e4af56a985dca506e3ea2c57c15fa9d0246401
Dmitrii <itdriver@yahoo.com>
2023-10-17 20:18:47 +0300

    pyramid

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-let k = 0;
-
-while (k < 5) {
-    k++;
-    console.log('k: ' + k);
-
+let d1 = document.querySelector('.d1');
+let c = 0;
+let res = '';
+let counter = 3;
+while (c < 4) {
+    let s = 0;
+    while (s < 4) {
+        if (counter > s) {
+            res += '-';
+        } else {
+            res += '*';
+        }
+        s++;
+    }
+    res += '<br>';
+    counter--;
+    c++;
 }
+
+d1.innerHTML = res;
