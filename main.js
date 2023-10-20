d7aa0ba50fced3ea9a6ea3c09c8b3164c8ded38c
Dmitrii <itdriver@yahoo.com>
2023-10-20 22:39:18 +0300

    d-array

--- main.js
+++
@@ -1,9 +0,0 @@
-const a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const b = ['a', 'b', 'c', 'd', 'e', 'f'];
-
-console.log(b.pop());
-
-console.log(a);
-// a.splice(0, 1, 23, 43);
-a.shift();
-console.log(a);
